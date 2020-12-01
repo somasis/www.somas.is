93703137e840f66e52b99f639f9246eb31a282a9
Kylie McClain <kylie@somas.is>
2020-11-30 21:39:52 -0500

    notes.sh: add description like rhizome, stop summaries after 5 posts

--- notes.sh
+++ notes.sh
@@ -5,11 +5,16 @@ set -e
 cat <<EOF
 title: index
 
+**notes** is a place for me to document and explain things so that I don't forget them, or to get
+thoughts out of my head that are bouncing around incessantly.
+
 [subscribe](notes.atom) in your favorite RSS reader.
 
 EOF
 
+i=0
 while [ $# -gt 0 ]; do
+    i=$(( i + 1 ))
     b="${1%.html}"
 
     title=$(lowdown -T html -X title "${b}".md)
@@ -17,8 +22,11 @@ while [ $# -gt 0 ]; do
     date=$(lowdown -T html -X date "${b}".md 2>/dev/null || :)
 
     echo "${date:+$date - }[${title}](${1})"
-    [ -n "${summary}" ] && echo ": ${summary}"
     echo
+    if [ "${i}" -lt 5 ]; then
+        [ -n "${summary}" ] && echo "> ${summary}"
+        echo
+    fi
     shift
 done
 
