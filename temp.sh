0dfd78bf405f9e26c3852e85bd4a325e61c5ea30
Kylie McClain <kylie@somas.is>
2021-05-14 16:35:39 -0400

    temp.sh: style, move sections to functions

--- temp.sh
+++ temp.sh
@@ -6,46 +6,8 @@ meta() {
     lowdown -T html -X "${1}" "${2}" 2>/dev/null
 }
 
-f="${1}"
-b="${1%.md}"
-
-
-title=$(meta title "${f}" || printf '%s\n' "${b#*-}")
-summary=$(meta summary "${f}" || printf '')
-date=$(meta date "${f}" || printf '')
-
-site_name='~somasis' # Used for the beginning part of the ${header}, and the <title>.
-header="<a href='index.html'>${site_name}</a>" # Header displayed on top of <body>.
-page_title="${site_name}" # Passed directly to <title>.
-
-gravatar_img="https://www.gravatar.com/avatar/a187e38560bb56f5231cd19e45ad80f6?s=512"
-header_image="<img src='${gravatar_img}' />"
-
-case "${b}" in
-    rhizome-*)
-        site_name="${site_name}/rhizome"
-        page_title="${page_title}/rhizome - ${title}"
-        header="${header}/<a href='rhizome.html'>rhizome</a>"
-        ;;
-    note-*)
-        site_name="${site_name}/notes"
-        page_title="${page_title}/notes - ${title}"
-        header="${header}/<a href='notes.html'>notes</a>"
-        ;;
-    notes|rhizome)
-        site_name="${site_name}/${b}"
-        page_title="${page_title}/${b}"
-        header="${header}/${b}"
-        ;;
-    index)
-        header='~somasis'
-        ;;
-    *)
-        page_title="${page_title}${title:+ - $title}"
-        ;;
-esac
-
-cat <<EOF
+header() {
+    cat <<EOF
 <!DOCTYPE html>
 <html lang="en-US" xmlns:og="http://opengraphprotocol.org/schema/">
 <head>
@@ -79,9 +41,64 @@ cat <<EOF
 </header>
 <main>
 EOF
+}
+
+footer() {
+    cat <<EOF
+</main>
+<footer>
+<a href='https://git.mutiny.red/somasis/www.somas.is'>generated at $(TZ=UTC date +"%Y-%d-%mT%H:%M:%S%:z")</a>
+- <a href='mailto:kylie@somas.is${mailto_subject:+?subject=${mailto_subject}}'>kylie@somas.is</a>
+</footer>
+</body>
+</html>
+EOF
+}
+
+f="${1}"
+b="${1%.md}"
+
+title=$(meta title "${f}" || printf '%s\n' "${b#*-}")
+summary=$(meta summary "${f}" || printf '')
+date=$(meta date "${f}" || printf '')
+
+site_name='~somasis' # Used for the beginning part of the ${header}, and the <title>.
+header="<a href='index.html'>${site_name}</a>" # Header displayed on top of <body>.
+page_title="${site_name}" # Passed directly to <title>.
+
+gravatar_img="https://www.gravatar.com/avatar/a187e38560bb56f5231cd19e45ad80f6?s=512"
+header_image="<img src='${gravatar_img}' />"
 
 case "${b}" in
-    index|rhizome|notes) : ;;
+    rhizome-*)
+        site_name="${site_name}/rhizome"
+        page_title="${page_title}/rhizome - ${title}"
+        header="${header}/<a href='rhizome.html'>rhizome</a>"
+        ;;
+    note-*)
+        site_name="${site_name}/notes"
+        page_title="${page_title}/notes - ${title}"
+        header="${header}/<a href='notes.html'>notes</a>"
+        ;;
+    notes | rhizome)
+        site_name="${site_name}/${b}"
+        page_title="${page_title}/${b}"
+        header="${header}/${b}"
+        ;;
+    index)
+        header='~somasis'
+        ;;
+    *)
+        page_title="${page_title}${title:+ - $title}"
+        ;;
+esac
+
+mailto_subject=$(printf '%s\n' "${page_title}" | jq -Rr @uri)
+
+header
+
+case "${b}" in
+    index | rhizome | notes) : ;;
     rhizome-*)
         printf '<header>\n'
         printf '<h1><time datetime="%s">%s</time></h1>\n' "${title}" "${title}"
@@ -104,13 +121,4 @@ $(lowdown -T html --html-no-escapehtml --html-no-skiphtml "${f}")
 </article>
 EOF
 
-cat <<EOF
-</main>
-<footer>
-<a href='https://git.mutiny.red/somasis/www.somas.is'>generated at $(TZ=UTC date +"%Y-%d-%mT%H:%M:%S%:z")</a>
-- <a href='mailto:kylie@somas.is'>kylie@somas.is</a>
-</footer>
-</body>
-</html>
-EOF
-
+footer
